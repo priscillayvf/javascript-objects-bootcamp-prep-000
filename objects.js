21887707fe67dd049ad38dfa64df5cb84e59c365
Priscilla Firpo <priscillayf@gmail.com>
2019-07-28 20:47:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,5 +9,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
+  delete playlist.Ginuwine
 }
